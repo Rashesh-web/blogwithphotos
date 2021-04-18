b4e5a2203c6218a66c4ffa4dae53ff0cd1cf8db4
Rashesh-web <82595271+Rashesh-web@users.noreply.github.com>
2021-04-18 12:00:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ function SetDate()
   d.setDate(17);
 document.getElementById("Date").innerHTML = d;
 }
+function dog()
+{
+    window.alert("No matter how youre feeling,a little dog gonna love you.")
+}
+function flower()
+{
+   window.alert("If we could see the miracle of a single flower clearly our whole life would change.")
+}
+function mountain()
+{
+    window.alert("Going to the mountains is going home.")
+}
+function bird()
+{
+    window.alert("birds of a feather flock together.")
+}
